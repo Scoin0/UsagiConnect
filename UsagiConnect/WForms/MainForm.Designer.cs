cdc0bc507f54d3d925ae471fd2077a83010cc9cf
Scoin0 <john.anfernee.lewis@gmail.com>
2023-04-06 19:24:49 -1000

    MainForm looks a bit nicer & added more functionality to it

--- UsagiConnect/WForms/MainForm.Designer.cs
+++ UsagiConnect/WForms/MainForm.Designer.cs
@@ -31,45 +31,35 @@ namespace UsagiConnect.WForms
         private void InitializeComponent()
         {
             this.pnlLeft = new System.Windows.Forms.Panel();
-            this.btnExit = new System.Windows.Forms.Button();
             this.btnSettings = new System.Windows.Forms.Button();
             this.btnDashboard = new System.Windows.Forms.Button();
             this.pnlTopLeft = new System.Windows.Forms.Panel();
-            this.circularPictureBox1 = new UsagiConnect.WForms.CustomControls.CircularPictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
             this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.consoleBox = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.profileAvatar = new UsagiConnect.WForms.CustomControls.CircularPictureBox();
             this.pnlLeft.SuspendLayout();
             this.pnlTopLeft.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).BeginInit();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.profileAvatar)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlLeft
             // 
             this.pnlLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.pnlLeft.Controls.Add(this.btnExit);
+            this.pnlLeft.Controls.Add(this.pnlNav);
             this.pnlLeft.Controls.Add(this.btnSettings);
             this.pnlLeft.Controls.Add(this.btnDashboard);
             this.pnlLeft.Controls.Add(this.pnlTopLeft);
+            this.pnlLeft.Controls.Add(this.btnExit);
             this.pnlLeft.Location = new System.Drawing.Point(0, 0);
             this.pnlLeft.Name = "pnlLeft";
             this.pnlLeft.Size = new System.Drawing.Size(180, 577);
             this.pnlLeft.TabIndex = 0;
-            // 
-            // btnExit
-            // 
-            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
-            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnExit.FlatAppearance.BorderSize = 0;
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Font = new System.Drawing.Font("LT Asus", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
-            this.btnExit.Location = new System.Drawing.Point(0, 527);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(180, 50);
-            this.btnExit.TabIndex = 1;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = false;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnSettings
             // 
@@ -105,8 +95,8 @@ namespace UsagiConnect.WForms
             // 
             // pnlTopLeft
             // 
-            this.pnlTopLeft.Controls.Add(this.circularPictureBox1);
-            this.pnlTopLeft.Controls.Add(this.label1);
+            this.pnlTopLeft.Controls.Add(this.lbName);
+            this.pnlTopLeft.Controls.Add(this.profileAvatar);
             this.pnlTopLeft.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTopLeft.Location = new System.Drawing.Point(0, 0);
             this.pnlTopLeft.Name = "pnlTopLeft";
@@ -114,35 +104,34 @@ namespace UsagiConnect.WForms
             this.pnlTopLeft.TabIndex = 0;
             this.pnlTopLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTopLeft_MouseDown);
             // 
-            // circularPictureBox1
+            // lbName
             // 
-            this.circularPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.circularPictureBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.circularPictureBox1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.circularPictureBox1.BorderDashCap = System.Drawing.Drawing2D.DashCap.Triangle;
-            this.circularPictureBox1.BorderDashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
-            this.circularPictureBox1.BorderSize = 0;
-            this.circularPictureBox1.GradientAngle = 50F;
-            this.circularPictureBox1.ImageLocation = "https://a.ppy.sh/3702410?1623916581.jpeg ";
-            this.circularPictureBox1.Location = new System.Drawing.Point(40, 12);
-            this.circularPictureBox1.Name = "circularPictureBox1";
-            this.circularPictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.circularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.circularPictureBox1.TabIndex = 1;
-            this.circularPictureBox1.TabStop = false;
+            this.lbName.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbName.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.ForeColor = System.Drawing.Color.HotPink;
+            this.lbName.Location = new System.Drawing.Point(0, 115);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(180, 30);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "UsagiConnect";
+            this.lbName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbName.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label1
+            // btnExit
             // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(0, 115);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(180, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "UsagiConnect";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("LT Asus", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.btnExit.Location = new System.Drawing.Point(0, 527);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(180, 50);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // pnlTop
             // 
@@ -154,12 +143,62 @@ namespace UsagiConnect.WForms
             this.pnlTop.TabIndex = 1;
             this.pnlTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseDown);
             // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.pnlMain.Controls.Add(this.consoleBox);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlMain.Location = new System.Drawing.Point(180, 30);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(771, 547);
+            this.pnlMain.TabIndex = 2;
+            // 
+            // consoleBox
+            // 
+            this.consoleBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(53)))));
+            this.consoleBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.consoleBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.consoleBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.consoleBox.ForeColor = System.Drawing.Color.White;
+            this.consoleBox.Location = new System.Drawing.Point(0, 0);
+            this.consoleBox.Name = "consoleBox";
+            this.consoleBox.ReadOnly = true;
+            this.consoleBox.Size = new System.Drawing.Size(771, 547);
+            this.consoleBox.TabIndex = 0;
+            this.consoleBox.Text = "";
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.pnlNav.Location = new System.Drawing.Point(0, 145);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 1;
+            // 
+            // profileAvatar
+            // 
+            this.profileAvatar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.profileAvatar.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.profileAvatar.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.profileAvatar.BorderDashCap = System.Drawing.Drawing2D.DashCap.Triangle;
+            this.profileAvatar.BorderDashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.profileAvatar.BorderSize = 0;
+            this.profileAvatar.GradientAngle = 50F;
+            this.profileAvatar.ImageLocation = "https://a.ppy.sh/3702410?1623916581.jpeg ";
+            this.profileAvatar.Location = new System.Drawing.Point(40, 12);
+            this.profileAvatar.Name = "profileAvatar";
+            this.profileAvatar.Size = new System.Drawing.Size(100, 100);
+            this.profileAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.profileAvatar.TabIndex = 1;
+            this.profileAvatar.TabStop = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.pnlMain);
             this.Controls.Add(this.pnlLeft);
             this.Controls.Add(this.pnlTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -169,7 +208,8 @@ namespace UsagiConnect.WForms
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.pnlLeft.ResumeLayout(false);
             this.pnlTopLeft.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).EndInit();
+            this.pnlMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.profileAvatar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -179,10 +219,14 @@ namespace UsagiConnect.WForms
         private System.Windows.Forms.Panel pnlLeft;
         private System.Windows.Forms.Panel pnlTop;
         private System.Windows.Forms.Panel pnlTopLeft;
-        private System.Windows.Forms.Label label1;
-        private CustomControls.CircularPictureBox circularPictureBox1;
+        private System.Windows.Forms.Label lbName;
+        private CustomControls.CircularPictureBox profileAvatar;
         private System.Windows.Forms.Button btnDashboard;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.RichTextBox consoleBox;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel pnlNav;
     }
 }
--- UsagiConnect/WForms/MainForm.cs
+++ UsagiConnect/WForms/MainForm.cs
@@ -8,7 +8,12 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
+using UsagiConnect.Client;
+using UsagiConnect.Osu.API.Enums;
+using UsagiConnect.Osu.API.User;
+using Button = System.Windows.Forms.Button;
 
 namespace UsagiConnect.WForms
 {
@@ -20,15 +25,40 @@ namespace UsagiConnect.WForms
 
         [DllImport("user32.dll", EntryPoint = "SendMessage")]
         private static extern void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        
+        public OsuClient Client;
+        private Form activeForm;
+        private Button currentButton;
 
         public MainForm()
         {
             InitializeComponent();
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private async void MainForm_Load(object sender, EventArgs e)
         {
-            Log.Info("T");
+            Log.Info("Welcome to UsagiConnect!");
+            
+            User user = new User();
+            user = await Client.getUser("3702410", Gamemode.Osu);
+            profileAvatar.ImageLocation = user.AvatarUrl;
+            lbName.Text = user.Username;
+
+            if (Client.IsOnline)
+            {
+                profileAvatar.BorderColor = Color.Green;
+                profileAvatar.BorderColor2 = Color.Green;
+                profileAvatar.BorderSize = 2;
+            }
+            else 
+            {
+                profileAvatar.BorderColor = Color.Red;
+                profileAvatar.BorderColor2 = Color.Red;
+                profileAvatar.BorderSize = 2;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -37,9 +67,21 @@ namespace UsagiConnect.WForms
 
         private void btnDashboard_Click(object sender, EventArgs e)
         {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+                pnlNav.Height = btnDashboard.Height;
+                pnlNav.Top = btnDashboard.Top;
+                pnlNav.Left = btnDashboard.Left;
+            }
         }
         private void btnSettings_Click(object sender, EventArgs e)
         {
+            pnlNav.Height = btnSettings.Height;
+            pnlNav.Top = btnSettings.Top;
+            pnlNav.Left = btnSettings.Left;
+            OpenChildForm(new SettingsForm(), sender);
+            ActivateButton(sender);
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -57,6 +99,50 @@ namespace UsagiConnect.WForms
         {
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void OpenChildForm(Form childForm, object sender)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.pnlMain.Controls.Add(childForm);
+            this.pnlMain.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = Color.FromArgb(61, 61, 61);
+                    currentButton.ForeColor = Color.FromArgb(170, 170, 170);
+                    currentButton.Font = new System.Drawing.Font("LT Asus", 20.25f, System.Drawing.FontStyle.Bold);
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in pnlLeft.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(61, 61, 61);
+                    previousBtn.ForeColor = Color.FromArgb(170, 170, 170);
+                    previousBtn.Font = new System.Drawing.Font("LT Asus", 20.25f, System.Drawing.FontStyle.Bold);
+                }
+            }
         }
     }
 }
