c9484130fa84833e4ad3804501c7278309a8ffbd
Scoin0 <john.anfernee.lewis@gmail.com>
2023-04-13 21:37:51 -1000

    Convert integer time to Human Readable Time

---
+++ UsagiConnect/Utilities/ConvertTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UsagiConnect.Utilities
+{
+    public class ConvertTime
+    {
+        public static string HumanReadableTime(int seconds)
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            string formattedTime = "";
+
+            if (time.Hours > 0)
+            {
+                formattedTime += string.Format("{0:D}:{1:D2}:{2:D2}", time.Hours, time.Minutes, time.Seconds);
+            }
+            else if (time.Minutes > 0)
+            {
+                formattedTime += string.Format("{0:D}:{1:D2}", time.Minutes, time.Seconds);
+            }
+            else
+            {
+                formattedTime += string.Format("{0:D}", time.Seconds);
+            }
+            return formattedTime;
+        }
+    }
+}
