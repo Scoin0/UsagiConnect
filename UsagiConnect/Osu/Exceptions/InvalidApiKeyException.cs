be2776e1c676704db60a5d56c7d630ec54febd79
Scoin0 <john.anfernee.lewis@gmail.com>
2023-04-17 17:32:34 -1000

    InvalidApiKeyException

---
+++ UsagiConnect/Osu/Exceptions/InvalidApiKeyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UsagiConnect.Osu.Exceptions
+{
+    public class InvalidApiKeyException : Exception
+    {
+        public InvalidApiKeyException() { }
+
+        public InvalidApiKeyException(string message) : base(string.Format("Invalid API Key: {0}.", message)) { }
+    }
+}
