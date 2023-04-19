094395009a856144b035e188007fb1afcc9119c0
Scoin0 <john.anfernee.lewis@gmail.com>
2023-04-18 17:52:04 -1000

    Added BeatmapNotFoundException
    Probably doesn't work right.

---
+++ UsagiConnect/Osu/Exceptions/BeatmapNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UsagiConnect.Osu.Exceptions
+{
+    public class BeatmapNotFoundException : Exception
+    {
+        public BeatmapNotFoundException() { }
+
+        public BeatmapNotFoundException(string message) : base(string.Format("Beatmap Not Found: {0}.", message)) { }
+    }
+}
